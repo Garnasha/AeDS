e4c178e809214f60e4724aa652809a420dbb8983
Blackthifer <hessel96@msn.com>
2014-10-21 18:42:09 +0200

    some more fooling around

--- assignment1/wormholeCalc.cpp
+++ assignment1/wormholeCalc.cpp
@@ -4,9 +4,10 @@
 
 using namespace std;
 
-vector<unsigned int> paths(unsigned int i, unsigned int j, Matrix& input)
+vector<unsigned int>& paths(unsigned int i, unsigned int j, Matrix<unsigned int>& input)
 {
-    return NULL;
+    vector<unsigned int> values;
+    return values;
 }
 
 unsigned int minimum(vector<unsigned int>& values)
@@ -14,12 +15,17 @@ unsigned int minimum(vector<unsigned int>& values)
     return 0;
 }
 
-void calcWormholes(Matrix& input, vector< vector<unsigned int> >& tupleList)
+void calcWormholes(Matrix<unsigned int>& input, vector< vector<unsigned int> >& tupleList)
 {
-    vector<unsigned int[3]> wormholes;
+    vector< vector<unsigned int> > wormholes;
     for(vector<unsigned int> tuple : tupleList){
-        if(input[tuple[0]][tuple[1]] <= minimum(paths(tuple[0], tuple[1], input)){
-                wormholes.push_back({tuple[0], tuple[1], input[tuple[0]][tuple[1]]});
+        if(input[tuple[0]][tuple[1]] <= minimum(paths(tuple[0], tuple[1], input)))
+        {
+            vector<unsigned int> wormhole;
+            wormhole.push_back(tuple[0]);
+            wormhole.push_back(tuple[1]);
+            wormhole.push_back(input[tuple[0]][tuple[1]]);
+            wormholes.push_back(wormhole);
         }
     }
 }
