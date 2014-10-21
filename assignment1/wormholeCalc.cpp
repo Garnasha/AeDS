cd1e84b8dfcbb9d31aa5f996c9c6f9b7b14ccf02
Blackthifer <hessel96@msn.com>
2014-10-21 18:06:08 +0200

    more code

--- assignment1/wormholeCalc.cpp
+++ assignment1/wormholeCalc.cpp
@@ -1,7 +1,25 @@
 #include <cstdlib>
 #include <vector>
+#include <matrix.h>
 
-void wormholes(Matrix& input, vector<vector<unsigned int>> tupleList)
+using namespace std;
+
+vector<unsigned int> paths(unsigned int i, unsigned int j, Matrix& input)
 {
+    return NULL;
+}
 
+unsigned int minimum(vector<unsigned int>& values)
+{
+    return 0;
+}
+
+void calcWormholes(Matrix& input, vector< vector<unsigned int> >& tupleList)
+{
+    vector<unsigned int[3]> wormholes;
+    for(vector<unsigned int> tuple : tupleList){
+        if(input[tuple[0]][tuple[1]] <= minimum(paths(tuple[0], tuple[1], input)){
+                wormholes.push_back({tuple[0], tuple[1], input[tuple[0]][tuple[1]]});
+        }
+    }
 }
