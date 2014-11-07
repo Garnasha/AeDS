ffc19e908f328ea8660551688fa8f2c243855d58
Sal Wolffs <sal.wolffs@gmail.com>
2014-11-07 21:32:49 +0100

    added header incorrectly left out in last commit

--- assignment1/starchartIO.h
+++ assignment1/starchartIO.h
@@ -1,4 +1,21 @@
 #ifndef STARCHARTIO_H
 #define STARCHARTIO_H
 
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <limits>
+
+std::vector<std::vector<signed int>> read_adjacency_matrix(
+		std::istream &instream);
+
+void output_wormhole_list(
+		std::ostream &outstream,
+		std::vector<std::tuple<unsigned int,unsigned int,signed int>> links);
+
+std::vector<std::vector<signed int>> adjacencies_from_stdin();
+
+void wormholes_to_stdout(
+		std::vector<std::tuple<unsigned int,unsigned int,signed int>> links);
+
 #endif // STARCHARTIO_H
