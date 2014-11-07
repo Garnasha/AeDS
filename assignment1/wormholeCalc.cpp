b66f40813d5ca259f26b9333a922e788e541a8ca
Blackthifer <hessel96@msn.com>
2014-11-07 21:58:01 +0100

    ehhh?!?!?!

--- assignment1/wormholeCalc.cpp
+++ assignment1/wormholeCalc.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 typedef signed int distance;
 typedef unsigned int index;
-typedef vector< vector<signed int> > matrix;
-typedef tuple<unsigned int, unsigned int, signed int> wormhole;
+typedef vector<vector<distance>> matrix;
+typedef tuple<index, index, distance> wormhole;
 
-void addWormhole(vector<wormhole>& wormholes, unsigned int x, unsigned int y, int length)
+void addWormhole(vector<wormhole>& wormholes, index x, index y, distance length)
 {
     wormhole next = make_tuple(x, y, length);
     wormholes.push_back(next);
@@ -21,15 +21,15 @@ void addLastWormhole(matrix& input, matrix& deduced)
 
 }
 
-void deduceDistances(matrix& input, matrix& deduced, unsigned int x, unsigned int y)
+void deduceDistances(matrix& input, matrix& deduced, index x, index y)
 {
 
 }
 
-unsigned int indexForShortest(vector<int> row, unsigned int rowNr)
+index indexForShortest(vector<distance> row, index rowNr)
 {
-    unsigned int min = 0;
-    for(unsigned int j = rowNr + 1; j < row.size(); j++){
+    index min = 0;
+    for(index j = rowNr + 1; j < row.size(); j++){
         if(row[min] > row[j])
             min = j;
     }
@@ -38,10 +38,10 @@ unsigned int indexForShortest(vector<int> row, unsigned int rowNr)
 
 void fillMatrix(matrix& m, unsigned int size)
 {
-    for(unsigned int i = 0; i < size; i++){
+    for(index i = 0; i < size; i++){
         vector<int> row;
         m.push_back(row);
-        for(unsigned int j = 0; j < size; j++)
+        for(index j = 0; j < size; j++)
             m[i].push_back(INT_MAX);
     }
 }
@@ -51,8 +51,8 @@ void calcWormholes(matrix& input)
     matrix deduced;
     fillMatrix(deduced, input.size());
     vector<wormhole> wormholes;
-    for(unsigned int i = 0; i < input.size() - 1; i++){
-        unsigned int j = indexForShortest(input[i], i);
+    for(index i = 0; i < input.size() - 1; i++){
+        index j = indexForShortest(input[i], i);
         addWormhole(wormholes, i, j, input[i][j]);
         deduced[i][j] = input[i][j];
         deduceDistances(input, deduced, i, j);
